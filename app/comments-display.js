339fe5ec1f5d63e3f1960cd541632de58e0e156b
axelvair <axel.vair@laplateforme.io>
2023-03-15 12:11:52 +0100

    display comments without asynchrone display

--- app/comments-display.js
+++ app/comments-display.js
@@ -1,10 +1,17 @@
 const commentsDisplay = document.querySelector('#content-commentaires');
-
-fetch(`blog.php?article?id=12`)
+let id = window.location.search
+let param = new URLSearchParams(id)
+console.log(param.get('id'))
+fetch(`article.php?articleid=${param.get('id')}`)
     .then((response) => {
-        return response.text()
+        return response.json()
     })
-    .then((article) => {
-        commentsDisplay.textContent = article
+    .then((commentaires) => {
+        for(i = 0; i < commentaires.length; i++){
+            const newComment = document.createElement('div');
+            commentsDisplay.append(newComment);
+            console.log(commentaires[i]['contenu'])
+            newComment.textContent = commentaires[i]['contenu']
+        }
 
     })
--- article.php
+++ article.php
@@ -4,13 +4,18 @@ require_once ('src/Article.php');
 require_once ('src/User.php');
 require_once ('src/Comments.php');
 
+// Si GET['ID'] est set alors on instancie un nouvel objet article de la classe Article()
+// On appelle la fonction getUniqueArticle qui possède l'ID DE l'article afin d'afficher cette article en particulier
 if (isset($_GET['id'])) {
     $article = new Article();
     $article = $article->getUniqueArticle($_GET['id']);
-
-
 }
 
+if (isset($_GET['articleid'])) {
+    $commentaires = new Comments();
+    echo $commentaires->displayComments($_GET['articleid']);
+    die();
+}
 
 if(isset($_POST['commentaire'])){
     $contenu = htmlspecialchars($_POST['commentaire']);
