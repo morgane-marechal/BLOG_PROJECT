159d4908eecceff26fd116ba25e4bfa4919fe4cd
axelvair <axel.vair@laplateforme.io>
2023-03-20 16:59:14 +0100

    css unique article desktop

--- app/comments-display.js
+++ app/comments-display.js
@@ -10,7 +10,6 @@ let param = new URLSearchParams(id)
 // On ajoute un écouteur au bouton #envoie-commentaire pour envoyer un commentaire
 // On récupère le contenu du textarea #commentaire et on l'envoie au serveur
 // On récupère le commentaire envoyé et on l'affiche dans la div #content-commentaires
-// On vide le textarea #commentaire
 fetch(`article.php?articleid=${param.get('id')}`)
     .then((response) => {
         return response.json()
@@ -18,9 +17,11 @@ fetch(`article.php?articleid=${param.get('id')}`)
     .then((data) => {
         for (let i = 0; i < data.length; i++) {
             const div = document.createElement('div');
+            const spanInfo = document.createElement('span');
             div.classList.add('commentaire');
             div.innerHTML = data[i]['contenu'];
             commentsDisplay.appendChild(div);
+
         }
     })
 // Ecoute du bouton #envoie-commentaire
@@ -43,7 +44,6 @@ commentsForm.addEventListener('submit', (ev) => {
             const divCommentaire = document.createElement('div');
             divCommentaire.classList.add('commentaire');
             divCommentaire.innerHTML = data;
-            commentsDisplay.appendChild(divCommentaire);
-        })
+        }
     })
 
