67772fd2d9d437a73935ca36dabefeaddf83ace3
axelvair <axel.vair@laplateforme.io>
2023-03-21 13:42:33 +0100

    add form click at start authentification.php

--- authentification.php
+++ authentification.php
@@ -1,6 +1,8 @@
 <?php session_start();?>
 <head>
     <meta charset="utf-8"/>
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <script defer type="text/javascript" src="app/module_authentification.js"></script>
     <link rel="stylesheet" type="text/css" href="assets/style.css" />
         <meta http-equiv="x-ua-compatible" content="IE=edge" />
